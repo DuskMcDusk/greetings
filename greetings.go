cf29601395725cf92c3802bdbe22f224b9fa8921
Sven <s.baumgartner@tcommunication.it>
2025-11-12 14:54:18 +0100

    hello with error

--- greetings.go
+++ greetings.go
@@ -1,5 +1,14 @@
 package greetings
 
-func Hello(msg string) string {
-	return "Hello " + msg
+import (
+	"errors"
+	"fmt"
+)
+
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("msg cannot be empty")
+	}
+	message := fmt.Sprintf("Hello %s!", name)
+	return message, nil
 }
